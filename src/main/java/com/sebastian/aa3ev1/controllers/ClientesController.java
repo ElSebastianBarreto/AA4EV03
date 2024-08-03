03740beee35d3de19d2ebab6a67e598a87a468d1
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-08-03 10:02:03 -0500

    se implementa la funcion de guardar clientes

--- src/main/java/com/sebastian/aa3ev1/controllers/ClientesController.java
+++ src/main/java/com/sebastian/aa3ev1/controllers/ClientesController.java
@@ -9,6 +9,7 @@ import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -48,6 +50,15 @@ public class ClientesController {
 	{return repo.get(cedula);}
 	///
 	
+	//funcion de guardar y enviar al servidor
+	@PostMapping(value ="/guardar")
+	public ResponseEntity<Cliente> save(@RequestBody Cliente Cliente){
+		
+		Cliente obj = ResponseEntity.save(Cliente);
+		return new ResponseEntity<Cliente>(obj, HttpStatus.ok);
+	}
 
+	
+	
 }
 	
